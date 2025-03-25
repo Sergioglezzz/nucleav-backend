f69fd61b6483cbd04273e0298e7572057aaa2507
sgx <sergio.gonzalez@soologic.com>
2025-03-25 14:53:42 +0100

    feat: typeORM and entity user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -7,5 +8,15 @@ import { UserModule } from './user/user.module';
   imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
+})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true, // ⚠️ Solo en desarrollo, en producción usar migrations
+    }),
+  ],
 })
 export class AppModule {}
