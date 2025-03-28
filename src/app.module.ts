d96af36bbbd37c5b5b5e441de6a6a59713f0dc15
Sergio Gonzalez <sergioglez997@gmail.com>
2025-03-28 23:38:26 +0100

    fix: change the url in env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // Permite acceder a process.env en toda la app
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
       ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
