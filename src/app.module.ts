881a1024299e27c3e0da9fdbc88d3fe873c95687
Sergio Gonzalez <sergioglez997@gmail.com>
2025-03-28 23:15:56 +0100

    chore: tryin connection postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   isGlobal: true, // Permite acceder a process.env en toda la app
-    // }),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.DATABASE_HOST,
-    //   port: Number(process.env.DATABASE_PORT),
-    //   username: process.env.DATABASE_USER,
-    //   password: process.env.DATABASE_PASSWORD,
-    //   database: process.env.DATABASE_NAME,
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    //   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-    // }),
+    ConfigModule.forRoot({
+      isGlobal: true, // Permite acceder a process.env en toda la app
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    }),
   ],
   controllers: [],
   providers: [],
