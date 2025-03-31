29c11766fe172635cd8521cef514d0cc4dfd3cce
sgx <sergio.gonzalez@soologic.com>
2025-03-31 19:27:49 +0200

    feat: test api get form the back

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.use(
+    cors({
+      origin: ['https://nucleav.com'], // Ajusta según tu dominio en Vercel
+      credentials: true, // Permitir envío de cookies y headers de autenticación
+    }),
+  );
+
+  // Pipes de validación
+  app.useGlobalPipes(new ValidationPipe());
 
   // Configuración Swagger (opcional para documentación)
   const config = new DocumentBuilder()
@@ -14,7 +27,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap().catch((err) => {
   console.error('Error during bootstrap:', err);
