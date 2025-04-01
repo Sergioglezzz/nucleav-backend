fbd19464de51e1e6c9eccc01347c5e7974872acd
sgx <sergio.gonzalez@soologic.com>
2025-04-01 12:14:33 +0200

    fix: delete same api call in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import axios from 'axios';
 
 @Controller()
 export class AppController {
   @Get('external')
-  async getExternalData() {
-    try {
-      const response = await axios.get('https://api.nucleav.com/');
-      return response.data;
-    } catch (error) {
-      return { error: 'No se pudo obtener la información' };
-    }
+  getExternalData() {
+    return { message: 'Datos internos del backend' };
   }
 }
