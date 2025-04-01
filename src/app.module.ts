db2ae1371f59c59fc97b4e93d35729d7bb2a66f8
Sergio Gonzalez <sergioglez997@gmail.com>
2025-04-01 08:32:47 +0200

    chore: adapt the project to the production or development in back

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,6 @@
-// import { Module } from '@nestjs/common';
-// import { ConfigModule } from '@nestjs/config';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { TasksModule } from './tasks/tasks.module';
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({
-//       isGlobal: true,
-//     }),
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       url: process.env.DATABASE_URL,
-//       autoLoadEntities: true,
-//       synchronize: true,
-//       ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-//     }),
-//     TasksModule,
-//   ],
-//   controllers: [],
-//   providers: [],
-// })
-// export class AppModule {}
-
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 import { EmployeesModule } from './employees/employees.module';
@@ -32,17 +8,21 @@ import { Employee } from './employees/employees.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL, // Railway proporciona esta variable
-      synchronize: true, // ¡Solo para desarrollo! En producción usa migrations.
-      entities: [User, Employee], // O utiliza la opción de cargar todos los archivos en dist si lo prefieres
+      url: process.env.DATABASE_URL,
+      synchronize: process.env.NODE_ENV !== 'production', // Solo en desarrollo
       ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+      autoLoadEntities: true,
+      entities: [User, Employee],
     }),
     UsersModule,
     EmployeesModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
 
