7ff1871f140c9648dd97d7fe760ac6e97c0bf17f
sgx <sergio.gonzalez@soologic.com>
2025-04-23 14:23:01 +0200

    feat: jwt secret implement for login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Request,
+  Get,
+} from '@nestjs/common';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { Request as ExpressRequest } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
-
+export interface JwtPayload {
+  id: string;
+  email: string;
+  role: string;
+}
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   login(@Body() loginDto: LoginDto) {
@@ -15,5 +28,11 @@ export class AuthController {
   @Post('register')
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getMe(@Request() req: ExpressRequest): JwtPayload {
+    return req.user as JwtPayload;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,22 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      secret: 'yourSecretKey',
-      signOptions: { expiresIn: '1h' },
-    })
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
-export class AuthModule { }
+export class AuthModule {}
